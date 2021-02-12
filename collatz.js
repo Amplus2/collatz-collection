d8dab062ce6a716321ec9e597a1eb9ce34c67745
Luddifee <ludwig@ludwig-lehnert.de>
2021-02-12 22:06:59 +0100

    first diagrams and soma other small updates

--- collatz.js
+++ collatz.js
@@ -1,8 +1,6 @@
-const asmArgs = {};
-
 function range(start, end) {
-    var result = [];
-    if(end === undefined)
+    const result = [];
+    if (end === undefined)
         for (var i = 0; i < start; i++)
             result.push(i);
     else
@@ -11,20 +9,24 @@ function range(start, end) {
     return result;
 }
 
+function slow_wasm() {
+    return navigator.userAgent.toLowerCase().indexOf("firefox") !== -1;
+}
 
 async function get_wasm_exports(path) {
     return (await WebAssembly.instantiate(await (await fetch(path)).arrayBuffer())).instance.exports;
 }
 
 async function collatz_steps(path, range) {
+    if (slow_wasm()) return collatz_steps_js(range);
     const result = [];
     const exports = await get_wasm_exports(path);
     const collatz_steps = exports.collatz_steps;
-    for(var i = 0; i < range.length; i++) result.push(collatz_steps(range[i]));
+    for (var i = 0; i < range.length; i++) result.push(collatz_steps(range[i]));
     return result;
 }
 
-async function collatz_steps_js(range) {
+function collatz_steps_js(range) {
     function steps(input) {
         if (input < 1) return -1;
         var steps = 0;
@@ -36,5 +38,18 @@ async function collatz_steps_js(range) {
     }
     const result = [];
     for(var i = 0; i < range.length; i++) result.push(steps(range[i]));
+    return result;
+}
+
+
+async function collatz_amount_steps(path, range) {
+    const collatz_nums = await collatz_steps(path, range);
+    collatz_nums.sort((a, b) => a - b);
+    const result = new Map();
+    for (var i = 0; i < collatz_nums.length; i++) {
+        const a = collatz_nums[i];
+        if (!result.has(a)) result.set(a, 1);
+        else result.set(a, 1 + result.get(a));
+    }
     return result;
 }
