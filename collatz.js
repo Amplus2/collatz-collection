c30141cfc7fed02f9eeda8ea581d6903a244f5af
Luddifee <ludwig@ludwig-lehnert.de>
2021-02-12 19:46:26 +0100

    updated binary once again (stupid error)

--- collatz.js
+++ collatz.js
@@ -19,17 +19,19 @@ async function get_wasm_exports(path) {
 async function collatz_steps(path, range) {
     const result = [];
     const exports = await get_wasm_exports(path);
-    for(var i = 0; i < range.length; i++) result.push(exports.collatz_steps(BigInt(range[i])));
+    const collatz_steps = exports.collatz_steps;
+    for(var i = 0; i < range.length; i++) result.push(collatz_steps_c(range[i]));
     return result;
 }
 
 async function collatz_steps_js(range) {
     function steps(input) {
+        input = BigInt(input);
         if (input < 1) return -1;
         var steps = 0;
         while (true) {
             if(input == 1) return steps;
-            input = input % 2 == 1 ? 3 * input + 1 : input / 2;
+            input = input % 2n == 1n ? 3n * input + 1n : input / 2n;
             steps++;
         }
     }
