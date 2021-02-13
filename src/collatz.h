2056ddfa96aea66b2d4e6ffbc9e4a8abb6d27109
Luddifee <ludwig@ludwig-lehnert.de>
2021-02-13 16:46:03 +0100

    Revert "added collatz_batch_steps"
    This reverts commit a3dd8bae35d9259cd22a97eef42a763be9488404.

--- src/collatz.h
+++ src/collatz.h
@@ -2,6 +2,5 @@
 #include <map>
 #include <stdint.h>
 
-int collatz_steps(uint_least64_t);
 std::map<int, int> collatz_count(int start, int end);
-std::map<int, int> collatz_batch_steps(int start, int end);
+int collatz_steps(uint_least64_t);
