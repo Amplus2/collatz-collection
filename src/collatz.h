25a5e37e0779a2ca7f651e8ec8613801b00107f9
chrissx <chrissx@chrissx.de>
2021-02-13 18:21:53 +0100

    fixed header

--- src/collatz.h
+++ src/collatz.h
@@ -3,4 +3,4 @@
 
 int collatz_steps(uint_least64_t);
 void collatz_count(int start, int end, int *counts, int count_len);
-void collatz_batch_steps(int start, int end, int *steps, int steps_len);
+void collatz_batch_steps(int start, int end, int *steps);
