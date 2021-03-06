a2901be2c5134031a6faaf872a8725b468e954a0
chrissx <chrissx@chrissx.de>
2021-03-06 01:50:48 +0100

    lets just use ints, because its better that way in the wasm and we can pull it off in the js

--- src/collatz.cc
+++ src/collatz.cc
@@ -1,9 +1,7 @@
 #include "collatz.hh"
-#include <stdint.h>
 
-CCAPI int collatz_steps(double doublesarebad) {
-        uint_fast64_t i = doublesarebad;
-        int n;
+CCAPI i64 collatz_steps(i64 i) {
+        i64 n;
         for(n = 0; i > 1; n++) {
                 if(i % 2 == 0) i /= 2;
                 else           i *= 3, i += 1;
--- src/collatz.hh
+++ src/collatz.hh
@@ -1,10 +1,12 @@
 #pragma once
 #include <stdint.h>
 
+#define i64 uint_fast64_t
+
 #ifdef __cplusplus
 # define CCAPI extern "C"
 #else
 # define CCAPI
 #endif
 
-CCAPI int collatz_steps(double);
+CCAPI i64 collatz_steps(i64);
