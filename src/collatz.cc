00516b69e5fcf4acaac5688a34c775c56fe9dea7
chrissx <chrissx@chrissx.de>
2021-04-16 01:07:14 +0200

    ludwigs "optimization" was horrible shit and actually worsened performance under most conditions

--- src/collatz.cc
+++ src/collatz.cc
@@ -2,7 +2,7 @@
 
 namespace {
         static constexpr inline void collatz_step(i64 &i) {
-                i = (i % 2) * (3 * i + 1) + ((i+1) % 2) * (i / 2);
+                i = i % 2 ? 3 * i + 1 : i / 2;
         }
         static constexpr inline void sputs(str &bfr, cstr s) {
                 while(*s) *bfr++ = *s++;
