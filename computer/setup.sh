4c16b435a2aa66dce321ea0a60f18fa4910b9a98
chris <chrissx@chrissx.de>
2021-05-05 21:42:48 +0200

    Added a script for setting up the CCC.
    hopefully this works, i wont bother to check rn

---
+++ computer/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y vim git tmux curl xorg chromium python3-pip
+
+pip3 install pynput gpiozero
+
+git clone https://github.com/Amplus2/collatz-collection.git
+
+sudo adduser --gecos '' kekw-admin
+
+cat > .xinitrc <<EOF
+#!/bin/sh
+
+python3 /home/pi/collatz-collection/computer/keyboard_driver.py &
+
+exec chromium https://amplus2.github.io/collatz-collection/ --kiosk \
+              --start-fullscreen --fast --noerrdialogs --fast-start \
+              --disable-infobars --disable-features=TranslateUI \
+              --disable-translate --window-size=1920,1080
+EOF
+
+cat > .profile <<EOF
+#!/bin/sh
+exec startx
+EOF
