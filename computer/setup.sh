64ee81ad49c7cc2edc6c8b285182b8228988c4e6
chris <chrissx@chrissx.de>
2021-05-14 12:29:48 +0200

    actually working dynamic resolution

--- computer/setup.sh
+++ computer/setup.sh
@@ -16,10 +16,14 @@ cat > .xinitrc <<EOF
 
 python3 /home/pi/collatz-collection/computer/keyboard_driver.py &
 
+xrandr="$(xrandr | grep '*' | head -n 1 | awk '{ print $1 }')"
+
+wid="$(echo "$xrandr" | cut -dx -f1)"
+hei="$(echo "$xrandr" | cut -dx -f2)"
+
 exec chromium https://amplus2.github.io/collatz-collection/ --kiosk \
-              --start-fullscreen --fast --noerrdialogs --fast-start \
-              --disable-infobars --disable-features=TranslateUI \
-              --disable-translate --window-size=1920,1080
+    --start-fullscreen --fast --noerrdialogs --fast-start --disable-infobars \
+    --disable-features=TranslateUI --disable-translate --window-size=$wid,$hei
 EOF
 
 cat > .profile <<EOF
